90d6adb840c28c2d2866b70e6e12946e450f27d1
Chester Sng <chestersng@gmail.com>
2023-03-16 13:45:37 +0800

    Change to blacklisting instead of whitelisting

--- upload.php
+++ upload.php
@@ -1,14 +1,16 @@
 <?php
 
 // A list of permitted file extensions
-$allowed = array('png', 'jpg', 'gif','zip');
+// $allowed = array('png', 'jpg', 'gif','zip');
+// A list of disallowed file extensions
+$disallowed = array('php'); // prevent uploading of php files!
 
 if(isset($_FILES['upl']) && $_FILES['upl']['error'] == 0){
 
 	$extension = pathinfo($_FILES['upl']['name'], PATHINFO_EXTENSION);
 
-	if(!in_array(strtolower($extension), $allowed)){
-		echo '{"status":"error"}';
+	if(in_array(strtolower($extension), $disallowed)){
+		echo '{"status":"error, php files are not allowed"}';
 		exit;
 	}
 
